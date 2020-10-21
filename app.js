583fe3286f2b7ab579707bb22dbb35a45fcfbb3a
sangwon0724 <65121442+sangwon0724@users.noreply.github.com>
2020-10-21 20:49:05 +0900

    2d context

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d"); //canvas의 그림 그리기 기능을 가져옴
 
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 let painting = false;
 
 function stopPainting() {
   painting = false;
+}
+
+function startPainting() {
+  painting = true;
 }
 
 function onMouseMove(event) {
@@ -11,6 +18,14 @@ function onMouseMove(event) {
   //상위 요소에 relative가 없으면 컨텐츠의 절대값으로 설정
   const x = event.offsetX;
   const y = event.offsetY;
+
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
 
 function onMouseDown(event) {
@@ -25,7 +40,7 @@ if (canvas) {
   //canvas가 존재하는 경우
   //if(변수===null){}같이 쓰지말고 이렇게 쓰자
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
